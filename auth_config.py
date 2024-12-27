4163ad03c2b35d0fe2e26cd81aa73af14fa4d5d1
Kaushik Mahorker <kmahorker@gmail.com>
2024-12-26 19:43:06 -0800

    nit update

--- auth_config.py
+++ auth_config.py
@@ -7,7 +7,7 @@ def patch_gmail_scopes(scopes: List[str]) -> List[str]:
     # GMAIL Metadata scopes conflict with other scopes in production (gmail issue)
     return scopes + ["https://www.googleapis.com/auth/gmail.addons.current.message.metadata", "https://www.googleapis.com/auth/gmail.metadata"]
 
-OUTPUT = """{"message":"Token received successfully.","data":{"api_service":"gmail","token_type":"Bearer","access_token":"ya29.a0ARW5m76hyRB25rV-GQy62NedeTAvGFMhOPkYRmyNSD6kUbqhLDfvzRyWobBRLzd2XcC45_fDTNttJARBvFR2KQ_D0bulPqMVSfy-2Xu2isze6_10bfPQk_fPn94cuL35-NbQ7AHwUVwL38P_ODVMHNJpkbR-1xqClBCQiHcUSYk5QBSJyGSmofi8qd-q6g1f3JuUaCgYKAeoSARISFQHGX2Mi3wO7EKrXiE6pJk0n0xMQ0A0203","scope":["https://www.googleapis.com/auth/gmail.modify","https://www.googleapis.com/auth/gmail.addons.current.message.readonly","https://www.googleapis.com/auth/gmail.readonly","https://mail.google.com/","https://www.googleapis.com/auth/gmail.settings.sharing","https://www.googleapis.com/auth/gmail.labels","https://www.googleapis.com/auth/gmail.insert","https://www.googleapis.com/auth/gmail.addons.current.action.compose","https://www.googleapis.com/auth/gmail.compose","https://www.googleapis.com/auth/gmail.send","https://www.googleapis.com/auth/gmail.settings.basic","https://www.googleapis.com/auth/gmail.addons.current.message.action"],"expires_at":1735271627,"refresh_token":null}}""" # Fill in with response from https://wildcard-voker.vercel.app
+OUTPUT = """<PASTE AUTH OUTPUT HERE>""" # Fill in with response from https://wildcard-voker.vercel.app
 DATA = json.loads(OUTPUT)["data"]
 
 class AuthConfig:
